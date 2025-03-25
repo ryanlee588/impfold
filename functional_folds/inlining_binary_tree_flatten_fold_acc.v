d6c8d68cf9b3b08442c5a968d513cb73ba140dec
ryanlsh <ryan@u.yale-nus.edu.sg>
2025-03-25 13:11:47 +0800

    Added complete transformation process for binary tree flatten and list reverse

--- functional_folds/inlining_binary_tree_flatten_fold_acc.v
+++ functional_folds/inlining_binary_tree_flatten_fold_acc.v
@@ -1,19 +1,76 @@
+(* Start of binary_tree_flatten_transformation.v *)
+(* Version of 250325 *)
+
+(* ***************************** *)
+(* Imports *)
+
 Require Import List.
 
+(* ***************************** *)
 (* Type Defintions *)
 
-Inductive binary_tree(V : Type) : Type :=
+Inductive binary_tree (V : Type) : Type :=
   Leaf : binary_tree V
 | Node : V -> binary_tree V -> binary_tree V -> binary_tree V.
 
-(* ************************* *)
-(* Unfolding binary_tree_fold and inlining its definiens *)
+(* ***************************** *)
+(* list_fold_right *)
+
+Definition binary_tree_fold (V W : Type) (lea : W) 
+(nod : V -> W -> W -> W) (t : binary_tree V) : W :=
+  let fix visit t :=
+    match t with
+      Leaf _ =>
+      lea
+    | Node _ v t1 t2 =>
+      nod v (visit t1) (visit t2)
+    end
+  in visit t.
+
+(* ***************************** *)
+(* Start of Transformation process for binary_tree_flatten to binary_tree_flatten *)
+
+(** Binary Tree Flatten **)
+
+Fixpoint binary_tree_flatten (V : Type) (t : binary_tree V) : list V :=
+  match t with
+  | Leaf _ =>
+     nil
+  | Node _ v t1 t2 =>
+     v :: (binary_tree_flatten V t1) ++ (binary_tree_flatten V t2)
+  end.
+
+(** ************************* **)
+(** Representing binary_tree_flatten using binary_tree_fold **)
+
+Definition binary_tree_flatten_fold (V : Type) (t : binary_tree V) : list V :=
+  binary_tree_fold
+    V
+    (list V)
+    nil
+    (fun v ihl ihr => v :: ihl ++ ihr)
+    t.
+
+(** ************************* **)
+(** Changing codomain from List V to (List V -> List V) **)
+
+Definition binary_tree_flatten_fold_acc (V : Type) (t : binary_tree V) : list V :=
+  binary_tree_fold
+    V
+    (list V -> list V)
+    (fun acc => acc)
+    (fun v ihl ihr acc => v :: (ihl (ihr acc)))
+    t
+    nil.
+
+(** ************************* **)
+(** Unfolding binary_tree_fold and inlining its definiens **)
 
 Definition binary_tree_flatten_inlined_v1 (V : Type) (t : binary_tree V) : list V :=
   (let V := V in
    let W := list V -> list V in
    let lea := (fun acc => acc) in
-   let nod := (fun v h1 h2 acc => v :: (h1 (h2 acc))) in
+   let nod := (fun v ihl ihr acc => v :: (ihl (ihr acc))) in
    let t := t in
    let fix visit t :=
        match t with
@@ -24,8 +81,8 @@ Definition binary_tree_flatten_inlined_v1 (V : Type) (t : binary_tree V) : list 
        end
    in visit t) nil.
 
-(* ************************* *)
-(* Unfolding the outer let expressions and commuting the "let fix visit" and the application to nil *)
+(** ************************* **)
+(** Unfolding the outer let expressions and commuting the "let fix visit" and the application to nil **)
 
 Definition binary_tree_flatten_inlined_v2 (V : Type) (t : binary_tree V) : list V :=
   let fix visit t :=
@@ -33,12 +90,12 @@ Definition binary_tree_flatten_inlined_v2 (V : Type) (t : binary_tree V) : list 
       Leaf _ =>
       (fun acc => acc)
     | Node _ v t1 t2 =>
-      (fun v h1 h2 acc => v :: (h1 (h2 acc))) v (visit t1) (visit t2)
+      (fun v ihl ihr acc => v :: (ihl (ihr acc))) v (visit t1) (visit t2)
     end
   in visit t nil.
 
 (* ************************* *)
-(* Beta-reduction *)
+(** Beta-reduction **)
 
 Definition binary_tree_flatten_inlined_v3 (V : Type) (t : binary_tree V) : list V :=
   let fix visit t :=
@@ -50,8 +107,8 @@ Definition binary_tree_flatten_inlined_v3 (V : Type) (t : binary_tree V) : list 
     end
   in visit t nil.
 
-(* ************************* *)
-(* Commuting the match-expression and lambda-expressions *)
+(** ************************* **)
+(** Commuting the match-expression and lambda-expressions **)
 
 Definition binary_tree_flatten_inlined_v4 (V : Type) (t : binary_tree V) : list V :=
   let fix visit t := fun acc =>
@@ -63,8 +120,8 @@ Definition binary_tree_flatten_inlined_v4 (V : Type) (t : binary_tree V) : list 
     end
   in visit t nil.
 
-(* ************************* *)
-(* Transforming the explicit lambda chaining to a curried multi-parameter syntax *)
+(** ************************* **)
+(** Transforming the explicit lambda chaining to a curried multi-parameter syntax **)
 
 Definition binary_tree_flatten_inlined_v5 (V :  Type) (t : binary_tree V) : list V :=
   let fix visit t acc :=
@@ -76,20 +133,23 @@ Definition binary_tree_flatten_inlined_v5 (V :  Type) (t : binary_tree V) : list
     end
   in visit t nil.
 
-(* ************************* *)
-(* Lambda-lifting *)
+(** ************************* **)
+(** Lambda-lifting **)
 
-Fixpoint binary_tree_flatten_inlined_v6_aux (V : Type) (t : binary_tree V)
+Fixpoint binary_tree_flatten_v2_aux (V : Type) (t : binary_tree V)
 (acc : list V) : list V :=
   match t with
     Leaf _ =>
       acc
   | Node _ v t1 t2 =>
-    v :: (binary_tree_flatten_inlined_v6_aux V t1
-    (binary_tree_flatten_inlined_v6_aux V t2 acc))
+    v :: (binary_tree_flatten_v2_aux V t1
+    (binary_tree_flatten_v2_aux V t2 acc))
   end.
 
-Definition binary_tree_flatten_inlined_v6 (V : Type) (t : binary_tree V) : list V :=
-  binary_tree_flatten_inlined_v6_aux V t nil.
+Definition binary_tree_flatten_v2 (V : Type) (t : binary_tree V) : list V :=
+  binary_tree_flatten_v2_aux V t nil.
 
-(* ************************* *)
+(** ************************* **)
+(* ***************************** *)
+
+(* End of File *)
--- functional_folds/inlining_list_reverse_right_acc.v
+++ functional_folds/inlining_list_reverse_right_acc.v
@@ -1,5 +1,60 @@
+(* Start of list_reverse_rtl_transformation.v *)
+(* Version of 250325 *)
+
+(* ***************************** *)
+(* Imports *)
+
 Require Import List.
 
+(* ***************************** *)
+(* list_fold_right *)
+
+Definition list_fold_right (V W : Type) (base_case : W) (cons_case : V -> W -> W )(ls : list V) : W :=
+  let fix visit ls :=
+    match ls with
+    | nil => base_case
+    | l :: ls' => cons_case l (visit ls')
+    end
+  in visit ls.
+
+(* ***************************** *)
+(* Start of Transformation process for list_reverse to list_reverse_v2 *)
+
+(** List Reverse Right to Left **)
+Fixpoint list_reverse_rtl
+(V : Type)
+(ls : list V) : list V :=
+  match ls with
+  | nil => nil
+  | l :: ls' => 
+        list_reverse_rtl V ls' ++ (l :: nil)
+  end.
+
+(** ************************* **)
+(** Representing list_reverse_rtl using list_fold_right **)
+
+Definition list_reverse_right (V : Type)
+(ls : list V) : list V :=
+  list_fold_right
+    V
+    (list V)
+    nil
+    (fun l ih => ih ++ (l :: nil))
+    ls.
+
+(** ************************* **)
+(** Changing codomain from List V to (List V -> List V) **)
+
+Definition list_reverse_right_acc (V : Type) (ls : list V) : list V :=
+  list_fold_right
+    V
+    (list V -> list V)
+    (fun acc => acc)
+    (fun l ih acc => ih (l :: acc))
+    ls
+    nil.
+
+(* ************************* *)
 (* Unfolding the call to list_fold_right and inlining is definiens *)
 
 Definition list_reverse_right_acc_inlined_v1 (V' : Type) (ls : list V') : list V' :=
@@ -15,8 +70,8 @@ Definition list_reverse_right_acc_inlined_v1 (V' : Type) (ls : list V') : list V
         end
    in visit ls) nil.
 
-(* ************************* *)
-(* Unfolding the outer let expressions and commuting the "let fix visit" and the application to nil *)
+(** ************************* **)
+(** Unfolding the outer let expressions and commuting the "let fix visit" and the application to nil **)
 
 Definition list_reverse_right_acc_inlined_v2 (V : Type) (ls : list V) : list V :=
   let fix visit ls := 
@@ -26,8 +81,8 @@ Definition list_reverse_right_acc_inlined_v2 (V : Type) (ls : list V) : list V :
     end
   in visit ls nil.
 
-(* ************************* *)
-(* Beta Reduction *)
+(** ************************* **)
+(** Beta Reduction **)
 
 Definition list_reverse_right_acc_inlined_v3 (V : Type) (ls : list V) : list V :=
   let fix visit ls := 
@@ -37,8 +92,8 @@ Definition list_reverse_right_acc_inlined_v3 (V : Type) (ls : list V) : list V :
     end
   in visit ls nil.
 
-(* ************************* *)
-(* Commuting the match-expression and the lambda-abstraction *)
+(** ************************* **)
+(** Commuting the match-expression and the lambda-abstraction **)
 
 Definition list_reverse_right_acc_inlined_v4 (V : Type) (ls : list V) : list V :=
   let fix visit ls := fun acc =>
@@ -48,8 +103,8 @@ Definition list_reverse_right_acc_inlined_v4 (V : Type) (ls : list V) : list V :
     end
   in visit ls nil.
 
-(* ************************* *)
-(* Transforming the explicit lambda chaining to a curried multi-parameter syntax *)
+(** ************************* **)
+(** Transforming the explicit lambda chaining to a curried multi-parameter syntax **)
 
 Definition list_reverse_right_acc_inlined_v5 (V : Type) (ls : list V) : list V :=
   let fix visit ls acc := 
@@ -59,16 +114,18 @@ Definition list_reverse_right_acc_inlined_v5 (V : Type) (ls : list V) : list V :
     end
   in visit ls nil.
 
-(* ************************* *)
-(* Lambda-lifting *)
-Fixpoint list_reverse_right_acc_inlined_v6_aux (V : Type)
-(ls : list V) (acc : list V) : list V :=
+(** ************************* **)
+(** Lambda-lifting **)
+
+Fixpoint list_reverse_v2_aux (V : Type) (ls : list V) (acc : list V) : list V :=
     match ls with 
     | nil => acc
-    | l :: ls' => list_reverse_right_acc_inlined_v6_aux V ls' (l :: acc) 
+    | l :: ls' => list_reverse_v2_aux V ls' (l :: acc) 
     end.
 
-Definition list_reverse_right_acc_inlined_v6 (V : Type) (ls : list V) : list V :=
-     list_reverse_right_acc_inlined_v6_aux V ls nil.
-(* ************************* *)
+Definition list_reverse_v2 (V : Type) (ls : list V) : list V :=
+     list_reverse_v2_aux V ls nil.
+(** ************************* **)
+(* ***************************** *)
 
+(* End of File *)
